cd04a51ff8f45d3a1993cb36b1211d9a41254f5a
System <none>
2017-07-06 20:10:49 +0000

    🙅🌸 Updated with Glitch

--- server.js
+++ server.js
@@ -5,7 +5,9 @@
 var express = require('express');
 var app = express();
 var interp = require('line-interpolate-points');
+var d3n = require('d3-node');
 var d3 = require('d3');
+var svg2png = require('svg2png');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -19,11 +21,15 @@ app.get("/", function (request, response) {
 });
 
 app.get("/chart", function (request, response) {
+  // TODO: add the function selection support
   
-  console.log('query: ', request.query);
   
   var mn = parseInt(request.query.min) || 0;
   var mx = parseInt(request.query.max) || 200;
+  
+  // supported mimes: application/json, image/png, image/svg+xml
+  //             for: data              png        d3 svg
+  var mime = request.query.mime || 'image/svg+xml';
   
   // default to undefined
   var s = request.query.str || 'undefined';
@@ -37,9 +43,18 @@ app.get("/chart", function (request, response) {
   // interpolate to the number of chars in the string
   var interpolated = _interpolate(data, s);
   
-  // pack the interpolated points with the char as label.
-  
-  response.send({data: interpolated});
+  response.setHeader('Content-Type', mime);
+  if (mime == 'application/json') {
+    response.send({data: interpolated});
+  } else if (mime == 'image/png') {
+    _chart(interpolated);
+    
+    // convert the svg to a png
+    
+  } else {
+    // send the "raw" svg
+    response.send(_chart(interpolated));
+  }
 });
 
 function _generate(mn, mx, fxn=Math.sin) {
@@ -62,7 +77,57 @@ function _interpolate(data, str) {
   return d;
 }
 
+function _chart(data) {
+  // generate the chart svg
+  // THIS IS MIXING D3 and D3-NODE for not
+  // great reasons other than speed
+  var margin = {top: 30, right: 20, bottom: 30, left: 50};
+  var width = 700 - margin.left - margin.right;
+  var height = 270 - margin.top - margin.bottom;
+  var padding = 5;
 
+  var xScale = d3.scaleLinear().range([0, width]);
+  var yScale = d3.scaleLinear().range([height, 0]);
+  
+  xScale.domain(d3.extent(data, function(d) {return d.x}));
+  yScale.domain(d3.extent(data, function(d) {return d.y}));
+  
+  var node = new d3n();
+  
+  var svg = node.createSVG(width + margin.left + margin.right, height + margin.top + margin.bottom);
+  
+  svg.selectAll('text')
+      .data(data)
+      .enter()
+      .append('text')
+      .text(function(d) {
+        return d.label;
+      })
+      .attr("x", function(d) {
+          return xScale(d.x);  // Returns scaled location of x
+      })
+      .attr("y", function(d) {
+          return yScale(d.y);  // Returns scaled circle y
+      })
+      .attr("font_family", "sans-serif")  // Font type
+      .attr("font-size", "11px")  // Font size
+      .attr("fill", "darkgreen");   // Font color
+    
+    
+  var x_axis = d3.axisBottom(xScale);
+
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(x_axis);
+
+  svg.append("g")
+    .call(d3.axisLeft(yScale));
+
+  // bin the tick labels
+  svg.select('g').call(x_axis).selectAll("text").remove();
+  
+  return node.svgString();
+}
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
